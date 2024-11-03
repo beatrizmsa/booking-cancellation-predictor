17ee9d90428eeb88c9b037bb30e75416f90fcdd4
Beatriz Sá <beatrizsa14@gmail.com>
2024-11-03 17:44:47 +0000

    final commit

--- utils.py
+++ utils.py
@@ -14,7 +14,9 @@ from sklearn.svm import SVC
 from sklearn.metrics import roc_curve, auc, classification_report, confusion_matrix, accuracy_score
 from sklearn.feature_selection import SelectFromModel
 
-## Data Exploration and Preparation functions
+
+# -----------------------------------Data Exploration Functions-----------------------------------
+
 def boxplot_by_type_visualization(data, columns, title=''):
     columns = [col for col in columns if col != 'is_canceled']
     n_cols = min(4, len(columns))
@@ -33,6 +35,7 @@ def boxplot_by_type_visualization(data, columns, title=''):
     plt.tight_layout(rect=(0, 0, 1, 0.95))
     plt.suptitle(title, fontsize=16)
     plt.show()
+
 
 def crosstab_by_type_visualization(data, columns, title):
     n_cols = min(4, len(columns))
@@ -95,25 +98,27 @@ def barplot_visualization(data, columns, title):
     plt.show()
 
 
+# -----------------------------------Grid Search Models-----------------------------------
+
+# Function to identify the best hyperparameters for an MLPClassifier (Neural Network)
 def best_neural_network(X_train, y_train):
-    # Define the parameter grid for the Neural Network
     param_grid = {
         'activation': ['relu', 'tanh', 'logistic'],
         'alpha': [0.0001, 0.001, 0.01],
     }
-    # Initialize the MLPClassifier (Neural Network model)
     nn = MLPClassifier(max_iter=200, random_state=42)
 
-    # Set up GridSearchCV to find the best parameters
     grid_search = GridSearchCV(nn, param_grid, cv=5, scoring='roc_auc', n_jobs=-1)
 
-    # Fit the grid search to the training data
     grid_search.fit(X_train, y_train)
 
     print("Best parameters", grid_search.best_params_)
     print("Best Score", grid_search.best_score_)
 
     return grid_search.best_estimator_
+
+
+# Function to find the best hyperparameters for a RandomForestClassifier
 def best_random_forest(X_train, y_train):
     rfm = RandomForestClassifier()
     param_rfm = {'n_estimators': [100, 200, 300, 400, 500],
@@ -125,8 +130,11 @@ def best_random_forest(X_train, y_train):
     print("Best parameters", rfm_gs.best_params_)
     print("Best Score", rfm_gs.best_score_)
     return rfm_gs.best_estimator_
+
+
+# Function to find the optimal number of neighbors for a KNeighborsClassifier
 def best_k_for_KNN(X_train, y_train):
-    param_grid = {'n_neighbors': np.arange(3,152,2)}
+    param_grid = {'n_neighbors': np.arange(3, 152, 2)}
 
     knn = KNeighborsClassifier()
 
@@ -137,7 +145,11 @@ def best_k_for_KNN(X_train, y_train):
     print("Best Score", grid_search.best_score_)
     return grid_search.best_estimator_
 
-## Lasso
+
+# -----------------------------------Lasso-----------------------------------
+
+
+# Feature selection using Logistic Regression with L1 regularization
 def lasso_regularization(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
@@ -159,6 +171,11 @@ def lasso_regularization(X, y):
 
     return X_lasso, df_lasso
 
+
+# -----------------------------------Evaluation Functions-----------------------------------
+
+
+# Function to print feature importances from Decision Tree
 def get_feature_importances_text(column_names, importances):
     features = list(zip(column_names, importances))
 
@@ -167,27 +184,23 @@ def get_feature_importances_text(column_names, importances):
     for feature, importance in features_sorted:
         print(f"- {feature}: {importance:.3f}")
 
-## Evaluation
+
+# Function to print a detailed classification report
 def classification_report_print(y_test, y_pred):
     report_dict = classification_report(y_test, y_pred, output_dict=True)
     df = pd.DataFrame(report_dict).transpose()
-
-    # Calcular a acurácia
     accuracy = accuracy_score(y_test, y_pred)
 
     df = df.round(2)
-    # Adicionar a linha de accuracy
     df.loc['accuracy'] = ["", "", round(accuracy, 2), len(y_test)]
+    df.loc[''] = ["", "", "", ""]
 
-    # Adicionar linha vazia
-    df.loc[''] = ["", "", "", ""]  # Linha vazia
-
-    # Reorganizar as linhas
     df = df.reindex(['0', '1', '', 'accuracy', 'macro avg', 'weighted avg'])
 
     return df
 
 
+# Function to plot a confusion matrix
 def plot_confusion_matrix(y_true, y_pred, suffix=''):
     title = 'Confusion Matrix '
     cm = confusion_matrix(y_true, y_pred)
@@ -201,7 +214,7 @@ def plot_confusion_matrix(y_true, y_pred, suffix=''):
     plt.show()
 
 
-
+# Function to generate ROC data for different models
 def roc_curve_data(models_dict, X_test, y_test):
     roc_data = {}
     for model_name, model in models_dict.items():
@@ -215,16 +228,15 @@ def roc_curve_data(models_dict, X_test, y_test):
     return roc_data
 
 
+# Function to create de plot for the roc curve
 def roc_curve_visualization(roc_data):
     plt.figure(figsize=(10, 8))
 
     for model_name, (fpr, tpr, auc_score) in roc_data.items():
         plt.plot(fpr, tpr, label=f'{model_name} (AUC = {auc_score:.2f})')
 
-    # Random guessing line
     plt.plot([0, 1], [0, 1], color='red', linestyle='--', label='Random Guessing')
 
-    # Labels and legend
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     plt.title('ROC Curve - Ensemble and Individual Models')
